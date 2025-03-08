7802da9ae446cf36a79dea28d405d935ac331c5e
jalsop24 <53493046+jalsop24@users.noreply.github.com>
2025-03-08 23:25:54 +0000

    Expose full curl result

--- cd/publish.sh
+++ cd/publish.sh
@@ -13,9 +13,7 @@ echo "Building place..."
 rojo build -o $PLACE_FILE
 
 echo "Publishing ${PLACE_FILE}"
-version="$(curl -s --location POST https://apis.roblox.com/universes/v1/$UNIVERSE_ID/places/$PLACE_ID/versions?versionType=Published \
+curl -s --location POST https://apis.roblox.com/universes/v1/$UNIVERSE_ID/places/$PLACE_ID/versions?versionType=Published \
     --header 'x-api-key: '$RBX_API_KEY \
     --header 'Content-Type: application/octet-stream' \
-    --data-binary @${PLACE_FILE})"
-
-echo "Place version: $(echo $version | jq .versionNumber)"
+    --data-binary @${PLACE_FILE}
