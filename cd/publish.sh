978ee8591d446e712df8401c488fe8890df7ba69
jalsop24 <53493046+jalsop24@users.noreply.github.com>
2025-03-08 23:41:14 +0000

    Update publish script

--- cd/publish.sh
+++ cd/publish.sh
@@ -13,7 +13,17 @@ echo "Building place..."
 rojo build -o $PLACE_FILE
 
 echo "Publishing ${PLACE_FILE} to universe '${UNIVERSE_ID}' / place '${PLACE_ID}'"
-curl -s --location POST https://apis.roblox.com/universes/v1/$UNIVERSE_ID/places/$PLACE_ID/versions?versionType=Published \
+response="$(curl -s --location POST https://apis.roblox.com/universes/v1/$UNIVERSE_ID/places/$PLACE_ID/versions?versionType=Published \
     --header 'x-api-key: '$RBX_API_KEY \
     --header 'Content-Type: application/octet-stream' \
-    --data-binary @${PLACE_FILE}
+    --data-binary @${PLACE_FILE})"
+
+version_number="$(echo $response | jq .versionNumber)"
+
+if [ "${version_number}" = "null" ]; then
+    echo "Failed to publish place ${response}"
+    exit 1
+fi
+
+echo "Place version: ${version_number}"
+
